74d78a943e89aac115bae3f2ff0bad8025d035d6
jose conde <a299506@uach.mx>
2023-04-03 12:27:32 -0600

    se crea archivo de reslucion del problema 2

---
+++ 2.js
@@ -0,0 +1,8 @@
+///2.- Hacer un programa que imprima los números del 100 al 0, en orden decreciente.
+
+function hundredToOne(){
+    for(let i=100; i>=0; i--){
+        console.log(i);
+    }
+}
+hundredToOne();
