d28f5afe993d15d8ccbcaa664cce172f46666d04
jose conde <a299506@uach.mx>
2023-04-03 17:16:33 -0600

    se crea archivo de resolucion del problema 23

---
+++ 23.js
@@ -0,0 +1,11 @@
+function zeroToHundredTab() {
+    for (let i = 0; i <= 100; i++) {
+      if (i % 10 == 0) {
+        console.log('\n');
+      }
+      process.stdout.write(`${i}     `);
+    }
+  }
+  
+  printNumbers();
+  
