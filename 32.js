a03b20f548c791e2ac0bd4f2aae54fb1832c6e1e
jose conde <a299506@uach.mx>
2023-04-03 20:06:39 -0600

    se crea archivo de resolucion del problema 32

---
+++ 32.js
@@ -0,0 +1,10 @@
+///32.- Crear un arreglo de 20 elementos con nombres de personas. Mostrar los elementos de la lista
+///debiendo ir cada uno en una fila distinta.
+
+let names = ["José", "Eduardo", "Anahí", "Fabian", "Manuel", "Fernanda", "Jessica", "Jesus", "Miriam", "Melissa", "Fernando", 
+            "Andres", "Rene", "Alejandro", "Gerardo", "Ubaldo", "Daniel", "Moises", "Luis", "Alan"];
+
+for(let i = 0; i < names.length; i ++ ){
+    console.log(names[i]);
+}
+
